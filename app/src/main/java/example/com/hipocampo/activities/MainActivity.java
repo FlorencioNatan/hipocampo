6fb4de9056bae99527f8eb6deeba002849e4161a
Florencio Natan <florencionatan@gmail.com>
2017-02-11 17:26:24 -0300

    Move MainActivity from package example.com.hipocampo to example.com.hipocampo.activities.
    modified:   app/src/main/AndroidManifest.xml
    renamed:    app/src/main/java/example/com/hipocampo/MainActivity.java -> app/src/main/java/example/com/hipocampo/activities/MainActivity.java
    modified:   app/src/main/res/layout/app_bar_main.xml
    modified:   app/src/main/res/layout/content_main.xml

--- app/src/main/java/example/com/hipocampo/activities/MainActivity.java
+++ app/src/main/java/example/com/hipocampo/activities/MainActivity.java
@@ -1,8 +1,9 @@
-package example.com.hipocampo;
+package example.com.hipocampo.activities;
 
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +13,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import example.com.hipocampo.R;
+import example.com.hipocampo.dialogs.MasterPasswordDialog;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -81,7 +85,9 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         if (id == R.id.nav_password) {
-            // Handle the camera action
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            MasterPasswordDialog dialog = new MasterPasswordDialog();
+            dialog.show(fragmentManager, "Master Password Dialog");
         }
 
 
